29e5c6ba4ab2006e6dda35bd628dda119ef83f3a
yadavpdsagar <130287779+yadavpdsagar@users.noreply.github.com>
2024-09-04 12:36:21 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,30 @@
 
 
 
-import { useState } from 'react'
+import {  useState } from 'react'
 
 import './App.css'
 
 function App() {
-  const [Color, setColor] = useState('olive');
+   const myColors = ["red", "blue", "green"]
+  const [color, setColor] = useState(myColors[0]);
 
+
+  const randomizeFunc = ()=>{
+   const index = Math.floor(Math.random()*3)
+   setColor(myColors[index])
+  }
+//   useEffect(()=>{
+//    randomizeFunc()
+//    }, [])
 
 
   return (
-   <div className='w-full shadow-lg text-black h-screen duration-200 bg-black ' style={{backgroundColor:Color}}>
-<div className='fixed fliex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+   <div className='w-full shadow-lg text-black h-screen duration-200  ' style={{backgroundColor:color}}>
+   <button onClick={()=> randomizeFunc()} className='bg-red'>Random color</button>
+{/* <div className='fixed fliex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+
+
   <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl ' >
 
   <button onClick={() => setColor('red')} className='outline-none px-4 py-1 rounded-full shadow-lg text-black'>
@@ -28,7 +40,7 @@ function App() {
     className=' outline-none px-4 py-1 rounded-full shadow-lg text-black '>Blue</button>
   </div>
 
-</div>
+</div> */}
    </div>
   )
 }
